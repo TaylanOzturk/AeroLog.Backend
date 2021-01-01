0168e7b8711daddb0f486d8c85782ff0e2406f7d
Hacı Taylan ÖZTÜRK <taylann95@gmail.com>
2021-01-01 19:57:45 +0300

    Proje dosyası ekle.

---
+++ AeroLog.Backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AeroLog.Backend.MvcApplication" Language="C#" %>
---
+++ AeroLog.Backend/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AeroLog.Backend.Models
+{
+    public class Users
+    {
+    }
+}
