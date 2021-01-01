1eb66f293a5be352f4ed30cfcfbdf63e54a7d119
Hacı Taylan ÖZTÜRK <taylann95@gmail.com>
2021-01-01 22:11:07 +0300

    user

--- AeroLog.Backend/Models/Users.cs
+++ AeroLog.Backend/Models/Users.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,12 @@ using System.Web;
 namespace AeroLog.Backend.Models
 {
     public class Users
+    {   
+        public int ID { get; set; }
+
+    }
+    public class UsersDBContext : DbContext
     {
+
     }
 }
